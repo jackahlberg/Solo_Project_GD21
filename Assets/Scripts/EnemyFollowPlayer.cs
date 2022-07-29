986d361de5ede1f49ca1a9fdbcc38ad225058185
jackahlberg <ahlberg.jack@gmail.com>
2022-07-29 19:15:32 +0200

    .

---
+++ Assets/Scripts/EnemyFollowPlayer.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class EnemyFollowPlayer : MonoBehaviour
+{
+
+    public float jumpHeight;
+    
+    public float speed;
+    private Transform _player;
+    public Rigidbody2D me;
+    public float groundCheckRadius;
+
+    private bool _jumpToRight;
+    private bool _jumpToLeft;
+    public bool _isGrounded;
+
+    [SerializeField] private LayerMask groundLayer;
+    void Start()
+    {
+        _player = GameObject.FindWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        _isGrounded = Physics2D.Raycast(me.position, Vector2.down, 1f, groundLayer);
+        _jumpToRight = Physics2D.Raycast(me.transform.position, Vector2.right, 1.5f, groundLayer);
+        _jumpToLeft = Physics2D.Raycast(me.transform.position, Vector2.left, 1.5f, groundLayer);
+
+        MoveTowards();
+        Jump();
+    }
+
+    private void MoveTowards()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, _player.transform.position, speed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        if (_jumpToLeft)
+        {
+            me.velocity = new Vector2(me.velocity.x, jumpHeight);
+        }
+        else if (_jumpToRight)
+        {
+            me.velocity = new Vector2(me.velocity.x, jumpHeight);
+        }
+        
+    }
+}
--- Assets/Scripts/MoveTowardsPlayer.cs
+++ Assets/Scripts/MoveTowardsPlayer.cs
@@ -7,7 +7,7 @@ public class MoveTowardsPlayer : MonoBehaviour
 {
     public float speed = 5;
     private Transform target;
-
+    
     private void Start()
     {
         target = GameObject.FindWithTag("Player").transform;
