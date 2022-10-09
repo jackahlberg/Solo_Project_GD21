a2718f9c1ed2087fdc2ab8a0e6279f56e6b14a0d
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-10-09 18:18:25 +0200

    Update InputManager.cs

--- Assets/Scripts/Universal/InputManager.cs
+++ Assets/Scripts/Universal/InputManager.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    public float walkInput;
+    public float HorizontalInput;
+    public float VerticalInput;
     public bool rollInput;
     public bool exitRollInput;
     public bool dashInput;
-    public bool jumpInput;
-    public bool jumpingInput;
+    public bool jumpDownInput;
+    public bool isJumping;
     public bool attackInput;
     public bool upInput;
     public bool downInput;
@@ -18,12 +19,13 @@ public class InputManager : MonoBehaviour
     
     private void GetInput()
     {
-        walkInput = Input.GetAxisRaw("Horizontal");
+        HorizontalInput = Input.GetAxisRaw("Horizontal");
+        VerticalInput = Input.GetAxisRaw("Vertical");
         rollInput = Input.GetKey(KeyCode.LeftControl);
         exitRollInput = Input.GetKey(KeyCode.LeftControl);
         dashInput = Input.GetKeyDown(KeyCode.LeftShift);
-        jumpInput = Input.GetKeyDown(KeyCode.Space);
-        jumpingInput = Input.GetButton("Jump");
+        jumpDownInput = Input.GetButtonDown("Jump");
+        isJumping = Input.GetButton("Jump");
         attackInput = Input.GetKeyDown(KeyCode.Mouse0);
         upInput = Input.GetKeyDown(KeyCode.W);
         downInput = Input.GetKeyDown(KeyCode.S);
