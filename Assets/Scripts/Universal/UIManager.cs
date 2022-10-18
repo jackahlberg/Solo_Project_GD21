5c3fbf853f0b8853fe40b9c1a71d5a13c1c9f4e5
jackahlberg <ahlberg.jack@gmail.com>
2022-10-18 09:10:14 +0100

    Update UIManager.cs

--- Assets/Scripts/Universal/UIManager.cs
+++ Assets/Scripts/Universal/UIManager.cs
@@ -6,13 +6,13 @@ public class UIManager : MonoBehaviour
     [SerializeField] private TMP_Text _healthText;
     [SerializeField] private TMP_Text _coinsText;
     private HealthManager _playerHealth;
-    private CurrencyContainer _playerCurrency;
+    private CurrencyManager _playerCurrency;
 
     
     private void Start()
     {
         _playerHealth = GameObject.FindWithTag("Player").GetComponent<HealthManager>();
-        _playerCurrency = GameObject.FindWithTag("Player").GetComponent<CurrencyContainer>();
+        _playerCurrency = GameObject.FindWithTag("Player").GetComponent<CurrencyManager>();
     }
 
     
